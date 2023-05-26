4e5b35add850e8f92815022c934fa0978eec4ef6
Yehor Podolskyi <66732525+yehorPo@users.noreply.github.com>
2023-05-26 14:23:23 +0300

    Initial Commit

---
+++ Inventory/InventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryApp.swift
+//  Inventory
+//
+//  Created by Yehor Podolskyi on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct InventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
